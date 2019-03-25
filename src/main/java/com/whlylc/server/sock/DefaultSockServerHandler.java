eec53be62e9c12ed1fc8a56e316195db95fbeda8
zealzeng <zealvampire@gmail.com>
2019-03-25 22:43:15 +0800

    Add ConnectionFuture for write method

--- src/main/java/com/whlylc/server/sock/DefaultSockServerHandler.java
+++ src/main/java/com/whlylc/server/sock/DefaultSockServerHandler.java
@@ -2,12 +2,14 @@ package com.whlylc.server.sock;
 
 import com.whlylc.server.ChannelServiceInboundHandler;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 
 /**
  * Default implementation of socket server handler
  * Created by Zeal on 2018/10/21 0021.
  */
+@ChannelHandler.Sharable
 public class DefaultSockServerHandler extends ChannelServiceInboundHandler<SockService,SockConnection,SockRequest,SockResponse,ByteBuf> {
 
 
