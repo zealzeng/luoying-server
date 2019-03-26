421db08eacacff293a5e6a048dc9781f4e0a19c7
zealzeng <zealvampire@gmail.com>
2019-03-26 22:09:46 +0800

    Upgrade to v0.1.4

--- src/main/java/com/whlylc/server/MultiChannelServer.java
+++ src/main/java/com/whlylc/server/MultiChannelServer.java
@@ -1,5 +1,8 @@
 package com.whlylc.server;
 
+import com.whlylc.ioc.ApplicationContext;
+import com.whlylc.ioc.DisposableBean;
+import com.whlylc.ioc.InitializingBean;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
@@ -34,6 +37,8 @@ public class MultiChannelServer {
     protected EventLoopGroup workerGroup = null;
 
     protected Set<Service> services = null;
+
+    protected Set<ApplicationContext> applicationContexts = null;
 
     protected LogLevel logLevel = LogLevel.INFO;
 
@@ -83,13 +88,37 @@ public class MultiChannelServer {
                     services.add(service);
                 }
             }
+
+            //FIXME Optimize the codes in this class while having time
+            //Initialize application contexts
+            applicationContexts = new HashSet<>(services.size());
+            for (Service service : services) {
+                ApplicationContext applicationContext = service.getServiceContext().getApplicationContext();
+                if (applicationContext != null) {
+                    applicationContexts.add(applicationContext);
+                }
+            }
+            for (ApplicationContext applicationContext : applicationContexts) {
+                for (String beanName : applicationContext.getBeanNames()) {
+                    Object bean = applicationContext.getBean(beanName);
+                    if (bean != null && (bean instanceof InitializingBean)) {
+                        try {
+                            ((InitializingBean) bean).initialize();
+                        }
+                        catch (Throwable t) {
+                            logger.error("Failed to initialize bean " + beanName, t);
+                        }
+                    }
+                }
+            }
+
             //Shutdown hook
             addShutdownHook();
             if (logger.isInfoEnabled()) {
                 logger.info("======Server is started======");
             }
             //ch.closeFuture().sync();
-        } catch (Exception e) {
+        } catch (Throwable e) {
             shutdown();
             throw e;
         }
@@ -106,8 +135,26 @@ public class MultiChannelServer {
         });
     }
 
-    private void destroyApplicationServices() {
-        if (services != null) {
+    private void destroyApplicationContexts() {
+        if (applicationContexts != null && applicationContexts.size() > 0) {
+            for (ApplicationContext applicationContext : applicationContexts) {
+                for (String beanNam : applicationContext.getBeanNames()) {
+                    Object bean = applicationContext.getBean(beanNam);
+                    if (bean != null && bean instanceof DisposableBean) {
+                        try {
+                            ((DisposableBean) bean).destroy();
+                        }
+                        catch (Throwable t) {
+                            logger.warn("Failed to destroy bean " + beanNam, t);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void destroyServices() {
+        if (services != null && services.size() > 0) {
             for (Service application : this.services) {
                 try {
                     application.destroy();
@@ -124,8 +171,10 @@ public class MultiChannelServer {
      */
     public void shutdown() {
 
+        //Destroy application contexts
+        this.destroyApplicationContexts();
         //Destroy services
-        this.destroyApplicationServices();
+        this.destroyServices();
         //Release threads
         if (bossGroup != null) {
             bossGroup.shutdownGracefully();
